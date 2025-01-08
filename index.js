3f38e54e61cccffbff4227919fd094a2c41dce00
Emeka <emekaamaechi0@gmail.com>
2025-01-08 21:29:01 +0100

    installed express and accessed resource using express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello World!')
+const express = require('express');
+const app = express();
+
+app.get('/', ({req, res}) => {
+res.status(200).send('HomePage')
+})
+
+
+app.listen(5000,() => {
+    console.log('Server is running at port 5000...')
+})
