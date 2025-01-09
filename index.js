d368dbf8efebaa13a866bf0d66490fc66f082394
Emeka <emekaamaechi0@gmail.com>
2025-01-09 21:22:38 +0100

    wrote my first api

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
+const people = require('./data')
 
-app.get('/', ({req, res}) => {
-res.status(200).send('HomePage')
+app.use(express.urlencoded({extended:false}))
+app.get('/', (req, res) => {
+res.status(200).json({success:true, data:people})
 })
 
+app.get('*', (req, res) => {
+    res.status(400).send('<h1>Resource Not Found<h1>')
+})
 
 app.listen(5000,() => {
     console.log('Server is running at port 5000...')
